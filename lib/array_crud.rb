f24f9463d7d27327bfb7c15a1155624ab25e393d
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-29 01:44:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(create_an_array)
 end
 
 def add_element_to_start_of_array(create_an_array)
-  create_an_array.unshift("arrays!")
+  create_an_array.push("arrays!")
 end
 
 def remove_element_from_end_of_array(create_an_array)
