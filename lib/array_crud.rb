b1d5d115956a59b0cc7729fe25d669e163487055
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-29 01:34:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["blue", "green", "purple", "orange"]
+  array.new(4)
 end
 
 def add_element_to_end_of_array(create_an_array, color = "yellow")
