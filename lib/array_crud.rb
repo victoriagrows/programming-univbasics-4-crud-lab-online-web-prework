80363b7220a243dcac2a6b053b365c1dd5e7f9bf
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-29 01:06:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(create_an_array, color = "yellow")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift
 end
 
 def remove_element_from_end_of_array(array)
