7078f219e2042b5c7df680afeedbfadc02d54c41
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-29 01:47:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,7 @@ end
 
 def create_an_array
   #Array.new(4)
-  create_an_array = ["wow", "I", "am", "really", "learning"]
+  create_an_array = ["wow", "I", "am", "really"]
 end
 
 def add_element_to_end_of_array(create_an_array)
