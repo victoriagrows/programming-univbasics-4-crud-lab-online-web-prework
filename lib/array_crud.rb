0bf6d38db7a6598340180ec03b92e7c9068bfc34
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-29 01:42:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   Array.new(4)
 end
 
-def add_element_to_end_of_array(create_an_array, color = "yellow")
+def add_element_to_end_of_array(create_an_array)
   create_an_array = ["wow", "I", "am", "really", "learning"]
 end
 
